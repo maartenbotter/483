248f6ce7f677f3c508282ec533a06c3c091bef43
Maarten Botter <mbotter16@gmail.com>
2020-02-14 11:54:34 +0100

    Update ListClassesApp, create ScrambleApp

--- Week 2/ListClassesApp/Program.cs
+++ Week 2/ListClassesApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace ScrambleApp
@@ -12,14 +13,14 @@ namespace ScrambleApp
             Console.WriteLine("Available assemblies:");
             var currentAssemblies = AppDomain.CurrentDomain.GetAssemblies();
             Dictionary<int, string> assemblyNameDict = new Dictionary<int, string>();
-            for (int i = 0; i < currentAssemblies.Length; i++)
-            {
-                var dictEntryNumber = i + 1;
-                assemblyNameDict.Add(dictEntryNumber, currentAssemblies[i].FullName);
-                Console.WriteLine($"{dictEntryNumber}: {assemblyNameDict[dictEntryNumber]}");
-            }
+            assemblyNameDict = currentAssemblies.Select((assembly, i) => new { assembly, i }).ToDictionary(x => x.i, x => x.assembly.FullName);
+
+            foreach(var entry in assemblyNameDict)
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+
             Console.WriteLine("Choose a number");
             var userInput = Console.ReadLine();
+
             if (int.TryParse(userInput, out int intInput) && assemblyNameDict.ContainsKey(intInput)) 
                 ShowClassNames(assemblyNameDict[intInput]);
             else 
--- Week 2/ScrambleApp/Program.cs
+++ Week 2/ScrambleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace ScrambleApp
@@ -7,7 +8,28 @@ namespace ScrambleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Provide 8 arguments");
+            var arguments = new List<string>();
+            for(int i = 0; i < 8; i++)
+            {
+                arguments.Add(Console.ReadLine());
+            }
+            Console.WriteLine($"Return: {ShuffleList(arguments)}");
+        }
+
+        private static string ShuffleList(List<string> inputList)
+        {
+            string shuffledListString = "";
+
+            Random r = new Random();
+            while (inputList.Count > 0)
+            {
+                int randomIndex = r.Next(0, inputList.Count);
+                shuffledListString += $"{inputList[randomIndex]} "; //add it to the new, random list
+                inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+            }
+
+            return shuffledListString; //return the new random list
         }
     }
 }
