5fbf7e249ff6a9e3f7e4b006b1119db5d782b2f3
Maarten Botter <mbotter16@gmail.com>
2020-02-14 10:52:13 +0100

    Add implemented 'ListClassesApp', add empty ScrambleApp project

---
+++ Week 2/ListClassesApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ScrambleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Available assemblies:");
+            var currentAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Dictionary<int, string> assemblyNameDict = new Dictionary<int, string>();
+            for (int i = 0; i < currentAssemblies.Length; i++)
+            {
+                var dictEntryNumber = i + 1;
+                assemblyNameDict.Add(dictEntryNumber, currentAssemblies[i].FullName);
+                Console.WriteLine($"{dictEntryNumber}: {assemblyNameDict[dictEntryNumber]}");
+            }
+            Console.WriteLine("Choose a number");
+            var userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out int intInput) && assemblyNameDict.ContainsKey(intInput)) 
+                ShowClassNames(assemblyNameDict[intInput]);
+            else 
+                Console.WriteLine("Invalid input");
+        }
+
+        private static void ShowClassNames(string assemblyName)
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load(assemblyName);
+                foreach (Type type in assembly.GetTypes())
+                {
+                    Console.WriteLine(type.FullName);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Invalid input, Stacktrace:\n{e.StackTrace}");
+            }
+        }
+    }
+}
---
+++ Week 2/ScrambleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace ScrambleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
