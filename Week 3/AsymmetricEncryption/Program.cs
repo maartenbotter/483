23c20c2f88f434c4c176a66896745caf63c84145
Maarten Botter <m.botter@betabit.nl>
2020-03-02 08:22:17 +0100

    Created console app for AES Encyption and RSA Encryption of input

---
+++ Week 3/AsymmetricEncryption/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsymmetricEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            // Get the public keyy   
+            var publicKey = rsa.ExportRSAPublicKey();
+            var privateKey = rsa.ExportRSAPrivateKey();
+            string input = string.Empty;
+            while (string.IsNullOrWhiteSpace(input))
+                input = Console.ReadLine();
+            EncryptText(publicKey, input, "AESencrypted.dat");
+            Console.WriteLine(PrintEncryptedData("AESencrypted.dat"));
+            Console.WriteLine(DecryptData(privateKey, "AESencrypted.dat"));
+        }
+
+        static void EncryptText(byte[] publicKey, string text, string fileName)
+        {
+            byte[] dataToEncrypt = new UnicodeEncoding().GetBytes(text);
+            byte[] encryptedData;
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportRSAPublicKey(publicKey, out int bytesRead);
+                encryptedData = rsa.Encrypt(dataToEncrypt, false);
+            }
+
+            File.WriteAllBytes(fileName, encryptedData);
+            Console.WriteLine("Data encrypted");
+        }
+
+        static string DecryptData(byte[] privateKey, string fileName)
+        {
+            byte[] dataToDecrypt = File.ReadAllBytes(fileName);
+            byte[] decryptedData;
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportRSAPrivateKey(privateKey, out int bytesRead);
+                decryptedData = rsa.Decrypt(dataToDecrypt, false);
+            }
+
+            return new UnicodeEncoding().GetString(decryptedData);
+        }
+
+        static string PrintEncryptedData(string fileName)
+        {
+            byte[] encrypted = File.ReadAllBytes(fileName);
+            using MemoryStream ms = new MemoryStream(encrypted);
+            using StreamReader reader = new StreamReader(ms);
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ Week 3/SymmetricEncryption/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SymmetricEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SymmetricAlgorithm aes = new AesManaged();
+            string input = string.Empty;
+            while (string.IsNullOrWhiteSpace(input))
+                input = Console.ReadLine();
+            EncryptText(aes, input, "AESencrypted.dat");
+            Console.WriteLine(PrintEncryptedData("AESencrypted.dat"));
+            Console.WriteLine(DecryptData(aes, "AESencrypted.dat"));
+        }
+
+        static void EncryptText(SymmetricAlgorithm aesAlgorithm, string text, string fileName)
+        {
+            byte[] encrypted;
+            ICryptoTransform encryptor = aesAlgorithm.CreateEncryptor(aesAlgorithm.Key, aesAlgorithm.IV);
+            using MemoryStream ms = new MemoryStream();
+            using CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+            using (StreamWriter sw = new StreamWriter(cs))
+                sw.Write(text);
+            encrypted = ms.ToArray();
+
+            File.WriteAllBytes(fileName, encrypted);
+            Console.WriteLine("Data encrypted");
+        }
+
+        static string DecryptData(SymmetricAlgorithm aesAlgorithm, string fileName)
+        {
+            ICryptoTransform decryptor = aesAlgorithm.CreateDecryptor(aesAlgorithm.Key, aesAlgorithm.IV);
+            byte[] encrypted = File.ReadAllBytes(fileName);
+            using MemoryStream ms = new MemoryStream(encrypted);
+            using CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+            using StreamReader sr = new StreamReader(cs);
+            return sr.ReadToEnd();
+        }
+
+        static string PrintEncryptedData(string fileName)
+        {
+            byte[] encrypted = File.ReadAllBytes(fileName);
+            using MemoryStream ms = new MemoryStream(encrypted);
+            using StreamReader reader = new StreamReader(ms);
+            return reader.ReadToEnd();
+        }
+    }
+}
